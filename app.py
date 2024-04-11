5ecca01827efd1ffdb7d5df8fabd47611b575ace
Abilash <abilashs003@gmail.com>
2024-04-11 08:48:04 +0000

    added comments

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ async def swap_faces(sourceImage: UploadFile = File(...), targetImage: UploadFil
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,host=0.0.0.0, port=8000)
+    uvicorn.run(app,host="0.0.0.0", port=8000)
